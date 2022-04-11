550123ce063e23ad804cb139bc20301f0716f512
Keyve Sampaio <keyve10@live.com>
2022-04-11 02:17:01 -0300

    Adicionar arquivos de projeto.

---
+++ Ex_ClasseAluno/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex_ClasseAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Digite o nome do aluno:");
+            aluno.Nome = (Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write($"Nota Final: {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine($"FALTARAM {aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)} PARA APROVAR NA MATÉRIA");
+            }
+        }
+    }
+}
